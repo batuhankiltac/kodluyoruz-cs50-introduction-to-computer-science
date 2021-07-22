f9ae74b5bc62206bcffef488c321bb29f157d06f
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-07-22 15:42:33 +0300

    Update MarioAdvanced.c

--- ProblemSet-1/MarioAdvanced.c
+++ ProblemSet-1/MarioAdvanced.c
@@ -1,38 +1,36 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int
-main (void)
+int main(void)
 {
-  int height;
-  do
+    int height;
+    do
     {
-      height = get_int ("Height: ");
-    }
-  while (height < 1 || height > 8);
-  if (height > 0 || height < 9)
+        height = get_int("Height: ");
+    } while (height < 1 || height > 8);
+    if (height > 0 || height < 9)
     {
-      int counter = 0;
-      for (int row = 0; row < height; row++)
-	{
-	  if (counter != height)
-	    {
-	      for (int spaces = (height - 1) - counter; spaces > 0; spaces--)
-		{
-		  printf (" ");
-		}
-	      for (int hashes = 0; hashes <= counter; hashes++)
-		{
-		  printf ("#");
-		}
-	      printf (" ");
-	      for (int hashes = 0; hashes <= counter; hashes++)
-		{
-		  printf ("#");
-		}
-	      printf ("\n");
-	      counter++;
-	    }
-	}
+        int counter = 0;
+        for (int row = 0; row < height; row++)
+        {
+            if (counter != height)
+            {
+                for (int spaces = (height - 1) - counter; spaces > 0; spaces--)
+                {
+                    printf(" ");
+                }
+                for (int hashes = 0; hashes <= counter; hashes++)
+                {
+                    printf("#");
+                }
+                printf(" ");
+                for (int hashes = 0; hashes <= counter; hashes++)
+                {
+                    printf("#");
+                }
+                printf("\n");
+                counter++;
+            }
+        }
     }
 }
