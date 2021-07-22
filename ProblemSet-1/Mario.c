09f9516c2f5fad7bb6589b0365ed98b1c3fd770c
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-07-22 15:42:11 +0300

    Update Mario.c

--- ProblemSet-1/Mario.c
+++ ProblemSet-1/Mario.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int
-main (void)
+int main(void)
 {
-  int n;
-  do
+    int n;
+    do
     {
-      n = get_int ("Height: ");
-    }
-  while (n < 1 || n > 8);
-  for (int i = 0; i < n; i++)
+        n = get_int("Height: ");
+    } while (n < 1 || n > 8);
+    for (int i = 0; i < n; i++)
     {
-      for (int j = n - 1; j > i; j--)
-	printf (" ");
-      for (int j = 0; j <= i; j++)
-	printf ("#");
-      printf ("\n");
+        for (int j = n - 1; j > i; j--)
+            printf(" ");
+        for (int j = 0; j <= i; j++)
+            printf("#");
+        printf("\n");
     }
 }
