f072d1365c748d061bb2214b39192349251ea129
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-07-22 15:56:59 +0300

    Update and rename MarioAdvanced(Python).py to marioadvanced.py

--- ProblemSet-6/marioadvanced.py
+++ ProblemSet-6/marioadvanced.py
@@ -1,5 +1,6 @@
 from cs50 import get_int
 
+
 def main():
     n = get_int("Enter a number between 1 and 8:\n")
     while n < 1 or n > 8:
@@ -11,12 +12,15 @@ def main():
             hashes(i)
             print()
 
+
 def spaces(n, i):
     for j in range(n, i + 1, -1):
         print(" ", end="")
 
+
 def hashes(i):
     for k in range(0, i + 1):
         print("#", end="")
+
 
 main()
