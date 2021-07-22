bee19b53fc05cf63d509a7c9d9db22a8e569232b
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-07-22 16:13:39 +0300

    Update helpers.py

--- ProblemSet-8/helpers.py
+++ ProblemSet-8/helpers.py
@@ -34,15 +34,13 @@ def login_required(f):
 
 def lookup(symbol):
     """Look up quote for symbol."""
-
-    # Contact API
     try:
-        response = requests.get(f"https://api.iextrading.com/1.0/stock/{urllib.parse.quote_plus(symbol)}/quote")
+        response = requests.get(
+            f"https://api.iextrading.com/1.0/stock/{urllib.parse.quote_plus(symbol)}/quote")
         response.raise_for_status()
     except requests.RequestException:
         return None
 
-    # Parse response
     try:
         quote = response.json()
         return {
