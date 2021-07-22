9ef34ed0c5a49f785e0b01dcf3ba5feb0daddc6a
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-07-22 15:59:05 +0300

    Update button.js

--- ProblemSet-8/static/button.js
+++ ProblemSet-8/static/button.js
@@ -1,39 +1,39 @@
-$(document).ready(function(){
-    $("button").click(function(){
+$(document).ready(function () {
+    $("button").click(function () {
 
         var stock = $(this).attr("value")
         var action = $(this).data('action');
-    	var quantity = prompt(`How many shares of ${stock} would you like to ${action}?`, 1);
+        var quantity = prompt(`How many shares of ${stock} would you like to ${action}?`, 1);
 
-    	if (quantity > 0) {
-    	    if (action == 'sell') {
-    	        alert(`${quantity} shares of ${stock} sold`);
-    	        $.ajax({
-            	    url: "/sell",
-            	    type: 'POST',
-            	    data: {
-            	        'stock': stock,
-            	        'shares': quantity
-            	    }
-            	});
-    	    }
-    	    else if (action == 'buy') {
-    		    alert(`${quantity} shares of ${stock} bought`);
-    		    $.ajax({
-            	    url: "/buy",
-            	    type: 'POST',
-            	    data: {
-            	        'stock': stock,
-            	        'shares': quantity
-            	    }
-            	});
-    	    }
-    	}
-    	else {
-    		alert(`You did not specify your number of shares of ${stock} to ${action}.`);
-    	}
+        if (quantity > 0) {
+            if (action == 'sell') {
+                alert(`${quantity} shares of ${stock} sold`);
+                $.ajax({
+                    url: "/sell",
+                    type: 'POST',
+                    data: {
+                        'stock': stock,
+                        'shares': quantity
+                    }
+                });
+            }
+            else if (action == 'buy') {
+                alert(`${quantity} shares of ${stock} bought`);
+                $.ajax({
+                    url: "/buy",
+                    type: 'POST',
+                    data: {
+                        'stock': stock,
+                        'shares': quantity
+                    }
+                });
+            }
+        }
+        else {
+            alert(`You did not specify your number of shares of ${stock} to ${action}.`);
+        }
 
 
-    	location.reload()
+        location.reload()
     });
 });
