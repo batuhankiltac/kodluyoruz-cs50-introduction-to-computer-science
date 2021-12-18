c9b8144918f5dfd49fb0c11042035cd46c00c92d
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-12-18 18:57:59 +0300

    Update resize.c

--- ProblemSet-3/resize.c
+++ ProblemSet-3/resize.c
@@ -2,34 +2,34 @@
 #include <stdlib.h>
 #include "bmp.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     char *infile = argv[2];
     char *outfile = argv[3];
-    if (argc != 4)
-    {
+
+    if (argc != 4) {
         fprintf(stderr, "Usage: ./resize n infile outfile\n");
         return 1;
     }
+
     int resize_factor = atoi(argv[1]);
-    if (resize_factor < 0 || resize_factor > 100)
-    {
+    if (resize_factor < 0 || resize_factor > 100) {
         fprintf(stderr, "n must be possitive and less than or equal to 100\n");
         return 1;
     }
+
     FILE *inptr = fopen(infile, "r");
-    if (inptr == NULL)
-    {
+    if (inptr == NULL) {
         fprintf(stderr, "Could not open %s.\n", infile);
         return 2;
     }
+
     FILE *outptr = fopen(outfile, "w");
-    if (outptr == NULL)
-    {
+    if (outptr == NULL) {
         fclose(inptr);
         fprintf(stderr, "Could not create %s.\n", outfile);
         return 3;
     }
+
     BITMAPFILEHEADER in_bf;
     BITMAPINFOHEADER in_bi;
     BITMAPFILEHEADER out_bf;
@@ -38,12 +38,14 @@ int main(int argc, char *argv[])
     fread(&in_bi, sizeof(BITMAPINFOHEADER), 1, inptr);
     if (in_bf.bfType != 0x4d42 || in_bf.bfOffBits != 54 || in_bi.biSize != 40 ||
         in_bi.biBitCount != 24 || in_bi.biCompression != 0)
+    
     {
         fclose(outptr);
         fclose(inptr);
         fprintf(stderr, "Unsupported file format.\n");
         return 4;
     }
+
     out_bf = in_bf;
     out_bi = in_bi;
     out_bi.biWidth = in_bi.biWidth * resize_factor;
@@ -54,48 +56,31 @@ int main(int argc, char *argv[])
         (out_bi.biWidth * sizeof(RGBTRIPLE) +
          out_padding) *
         abs(out_bi.biHeight);
-
     out_bf.bfSize = out_bi.biSizeImage + out_bf.bfOffBits;
     fwrite(&out_bf, sizeof(BITMAPFILEHEADER), 1, outptr);
     fwrite(&out_bi, sizeof(BITMAPINFOHEADER), 1, outptr);
 
-    for (int i = 0, biHeight = abs(in_bi.biHeight); i < biHeight; i++)
-    {
-
-        for (int v = 0; v < resize_factor; v++)
-        {
-
-            for (int j = 0; j < in_bi.biWidth; j++)
-            {
-
+    for (int i = 0, biHeight = abs(in_bi.biHeight); i < biHeight; i++) {
+        for (int v = 0; v < resize_factor; v++) {
+            for (int j = 0; j < in_bi.biWidth; j++) {
                 RGBTRIPLE triple;
-
                 fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
-
-                for (int h = 0; h < resize_factor; h++)
-                {
+                for (int h = 0; h < resize_factor; h++) {
                     fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
                 }
             }
 
             fseek(inptr, in_padding, SEEK_CUR);
-
-            for (int p = 0; p < out_padding; p++)
-            {
+            for (int p = 0; p < out_padding; p++) {
                 fputc(0x00, outptr);
             }
-
             fseek(inptr, -((in_bi.biWidth * sizeof(RGBTRIPLE)) + in_padding),
                   SEEK_CUR);
         }
-
         fseek(inptr, (in_bi.biWidth * sizeof(RGBTRIPLE)) + in_padding,
               SEEK_CUR);
     }
-
     fclose(inptr);
-
     fclose(outptr);
-
     return 0;
 }
