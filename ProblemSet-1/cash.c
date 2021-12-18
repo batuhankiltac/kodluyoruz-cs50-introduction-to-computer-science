a4e2c438e415820c206114e63b8da9a81654149d
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-12-18 18:46:38 +0300

    Update cash.c

--- ProblemSet-1/cash.c
+++ ProblemSet-1/cash.c
@@ -2,32 +2,30 @@
 #include <cs50.h>
 #include <math.h>
 
-int main(void)
-{
+int main(void) {
     int count = 0;
     float change;
-    do
-    {
+
+    do {
         change = get_float("Change owed: ");
-    } while (change < 0);
+    }
+    while (change < 0);
+    
     int cent = round(change * 100);
-    while (cent >= 25)
-    {
+    
+    while (cent >= 25) {
         cent = cent - 25;
         count++;
     }
-    while (cent >= 10)
-    {
+    while (cent >= 10) {
         cent = cent - 10;
         count++;
     }
-    while (cent >= 5)
-    {
+    while (cent >= 5) {
         cent = cent - 5;
         count++;
     }
-    while (cent >= 1)
-    {
+    while (cent >= 1) {
         cent = cent - 1;
         count++;
     }
