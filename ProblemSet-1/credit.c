73bfc13bd46ca78684ff37ed77c4f0e3bb22cedc
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-12-18 18:48:58 +0300

    Update credit.c

--- ProblemSet-1/credit.c
+++ ProblemSet-1/credit.c
@@ -2,24 +2,26 @@
 #include <cs50.h>
 #include <math.h>
 
-int main(void)
-{
+int main(void) {
     long long ccNumber;
-    do
-    {
+    
+    do {
         ccNumber = get_long_long("What's your credit card number?: ");
-    } while (ccNumber <= 0);
+    }
+    while (ccNumber <= 0);
+    
     int countDigits = 0;
     long long validation = ccNumber;
-    while (validation > 0)
-    {
+    
+    while (validation > 0) {
         validation = validation / 10;
         countDigits++;
     }
-    if (countDigits != 13 && countDigits != 15 && countDigits != 16)
-    {
+    
+    if (countDigits != 13 && countDigits != 15 && countDigits != 16) {
         printf("INVALID\n");
     }
+
     int digit1 = (((ccNumber % 100) / 10) * 2);
     int digit2 = (((ccNumber % 10000) / 1000) * 2);
     int digit3 = (((ccNumber % 1000000) / 100000) * 2);
@@ -28,6 +30,7 @@ int main(void)
     int digit6 = (((ccNumber % 1000000000000) / 100000000000) * 2);
     int digit7 = (((ccNumber % 100000000000000) / 10000000000000) * 2);
     int digit8 = (((ccNumber % 10000000000000000) / 1000000000000000) * 2);
+
     int check1 = ((digit1 % 10) + ((digit1 % 100) / 10));
     check1 = (check1 + ((digit2 % 10) + ((digit2 % 100) / 10)));
     check1 = (check1 + ((digit3 % 10) + ((digit3 % 100) / 10)));
@@ -36,6 +39,7 @@ int main(void)
     check1 = (check1 + ((digit6 % 10) + ((digit6 % 100) / 10)));
     check1 = (check1 + ((digit7 % 10) + ((digit7 % 100) / 10)));
     check1 = (check1 + ((digit8 % 10) + ((digit8 % 100) / 10)));
+
     int digit9 = (ccNumber % 10);
     int digit10 = ((ccNumber % 1000) / 100);
     int digit11 = ((ccNumber % 100000) / 10000);
@@ -44,6 +48,7 @@ int main(void)
     int digit14 = ((ccNumber % 100000000000) / 10000000000);
     int digit15 = ((ccNumber % 10000000000000) / 1000000000000);
     int digit16 = ((ccNumber % 1000000000000000) / 100000000000000);
+
     check1 = (check1 + ((digit9 % 10) + ((digit9 % 100) / 10)));
     check1 = (check1 + ((digit10 % 10) + ((digit10 % 100) / 10)));
     check1 = (check1 + ((digit11 % 10) + ((digit11 % 100) / 10)));
@@ -52,41 +57,36 @@ int main(void)
     check1 = (check1 + ((digit14 % 10) + ((digit14 % 100) / 10)));
     check1 = (check1 + ((digit15 % 10) + ((digit15 % 100) / 10)));
     check1 = (check1 + ((digit16 % 10) + ((digit16 % 100) / 10)));
-    if (check1 % 10 != 0)
-    {
+
+    if (check1 % 10 != 0) {
         printf("INVALID\n");
     }
+
     long long amex = (ccNumber / 10000000000000);
-    if (countDigits == 15)
-    {
-        if (amex != 34 && amex != 37)
-        {
+    
+    if (countDigits == 15) {
+        if (amex != 34 && amex != 37) {
             printf("INVALID\n");
         }
-        else
-        {
+        else {
             printf("AMEX\n");
         }
     }
-    if (countDigits == 13)
-    {
+    
+    if (countDigits == 13) {
         printf("VISA\n");
     }
-    if (countDigits == 16)
-    {
-        if ((digit8 / 2) == 4)
-        {
+    
+    if (countDigits == 16) {
+        if ((digit8 / 2) == 4) {
             printf("VISA\n");
         }
-        else
-        {
+        else {
             long long mastercard = (ccNumber / 100000000000000);
-            if (mastercard != 51 && mastercard != 52 && mastercard != 53 && mastercard != 54 && mastercard != 55)
-            {
+            if (mastercard != 51 && mastercard != 52 && mastercard != 53 && mastercard != 54 && mastercard != 55) {
                 printf("INVALID\n");
             }
-            else
-            {
+            else {
                 printf("MASTERCARD\n");
             }
         }
