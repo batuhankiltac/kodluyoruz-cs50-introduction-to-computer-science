6d241013bf9752c3e0302f5c413d36b566bec185
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-12-18 18:55:54 +0300

    Update recover.c

--- ProblemSet-3/recover.c
+++ ProblemSet-3/recover.c
@@ -3,20 +3,16 @@
 #include <stdint.h>
 
 typedef uint8_t BYTE;
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         printf("Usage: ./recover image\n");
         return 1;
     }
-    else
-    {
-
+    else {
         char *input_file_name = argv[1];
         FILE *input_pointer = fopen(input_file_name, "r");
-        if (input_pointer == NULL)
-        {
+        if (input_pointer == NULL) {
             printf("Error: cannot open %s\n", input_file_name);
             return 2;
         }
@@ -26,24 +22,16 @@ int main(int argc, char *argv[])
         FILE *img_pointer = NULL;
         char filename[8];
 
-        while (fread(&buffer, 512, 1, input_pointer) == 1)
-        {
-
-            if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
-            {
-
-                if (!(count == 0))
-                {
+        while (fread(&buffer, 512, 1, input_pointer) == 1) {
+            if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) {
+                if (!(count == 0)) {
                     fclose(img_pointer);
                 }
-
                 sprintf(filename, "%03i.jpg", count);
                 img_pointer = fopen(filename, "w");
                 count++;
             }
-
-            if (!(count == 0))
-            {
+            if (!(count == 0)) {
                 fwrite(&buffer, 512, 1, img_pointer);
             }
         }
