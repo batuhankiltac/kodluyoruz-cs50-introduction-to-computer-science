d4177a0523464f4a9b4d649580bfc9f9023d17ef
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-12-18 18:54:41 +0300

    Update vigenere.c

--- ProblemSet-2/vigenere.c
+++ ProblemSet-2/vigenere.c
@@ -3,51 +3,43 @@
 #include <string.h>
 #include <ctype.h>
 
-int main(int argc, string argv[])
-{
+int main(int argc, string argv[]) {
     int keyLength;
-    if (argc != 2)
-    {
+
+    if (argc != 2) {
         printf("error");
         return 1;
     }
-    else
-    {
+    else {
         keyLength = strlen(argv[1]);
-        for (int h = 0; h < keyLength; h++)
-        {
-            if (!isalpha(argv[1][h]))
-            {
+        for (int h = 0; h < keyLength; h++) {
+            if (!isalpha(argv[1][h])) {
                 printf("error");
                 return 1;
             }
         }
     }
+
     char key[keyLength];
-    for (int g = 0; g < keyLength; g++)
-    {
+    for (int g = 0; g < keyLength; g++) {
         key[g] = argv[1][g];
     }
+
     printf("plaintext:");
     string plainText = get_string("plaintext: ");
     char cipherText[strlen(plainText)];
     int plainLength = strlen(plainText);
-    for (int i = 0, j = 0; i < plainLength; i++)
-    {
-        if (isalpha(plainText[i]))
-        {
-            if (isupper(key[j % keyLength]))
-            {
-                if (isupper(plainText[i]))
-                {
+    for (int i = 0, j = 0; i < plainLength; i++) {
+        if (isalpha(plainText[i])) {
+            if (isupper(key[j % keyLength])) {
+                if (isupper(plainText[i])) {
                     cipherText[i] =
                         ((((plainText[i] - 65) +
                            (key[j % keyLength] - 65)) %
                           26) +
                          65);
                 }
-                else
-                {
+                else {
                     cipherText[i] =
                         ((((plainText[i] - 97) +
                            (key[j % keyLength] - 65)) %
@@ -55,18 +47,15 @@ int main(int argc, string argv[])
                          97);
                 }
             }
-            else
-            {
-                if (isupper(plainText[i]))
-                {
+            else {
+                if (isupper(plainText[i])) {
                     cipherText[i] =
                         ((((plainText[i] - 65) +
                            (key[j % keyLength] - 97)) %
                           26) +
                          65);
                 }
-                else
-                {
+                else {
                     cipherText[i] =
                         ((((plainText[i] - 97) +
                            (key[j % keyLength] - 97)) %
@@ -76,14 +65,13 @@ int main(int argc, string argv[])
             }
             j++;
         }
-        else
-        {
+        else {
             cipherText[i] = plainText[i];
         }
     }
     printf("ciphertext: ");
-    for (int k = 0; k < plainLength; k++)
-    {
+
+    for (int k = 0; k < plainLength; k++) {
         printf("%c", cipherText[k]);
     }
     printf("\n");
